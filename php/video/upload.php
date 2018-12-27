167760c2033adf112c9fb736cc9989cdf0e827d2
simon-isler <simon-isler@gmx.ch>
2018-12-27 15:26:49 +0100

    video db entry

--- php/video/upload.php
+++ php/video/upload.php
@@ -23,6 +23,8 @@ if (isset($_POST['hochladen'])) {
     $filename = $_FILES["file"]["name"];
     $kategorie = $_POST['kategorie'];
     $path = "clips/" . $filename;
+    $id = "NULL";
+    $date = date('Y-m-d');
 
     // validation
     if ((($_FILES["file"]["type"] == "video/mp4") || ($_FILES["file"]["type"] == "video/wma") || ($_FILES["file"]["type"] == "video/quicktime")) && ($_FILES["file"]["size"] < 1000000000) && in_array($extension, $allowedExts)) {
@@ -42,8 +44,9 @@ if (isset($_POST['hochladen'])) {
                 $conn = mysqli_connect("localhost", "root", "", "SportClips");
 
                 // database entry
-                $insert = $conn->prepare("INSERT INTO TVideos (VidID, VidName, VidTag, VidPath VidDatum, BenID) values (NULL, '$filename', '$kategorie', '$path', 'now()', '$benutzerId')");
-                $conn->query($insert);
+                $insert = $conn->prepare("INSERT INTO TVideos (VidID, VidName, VidTag, VidPath, VidDatum, BenID) values (?, ?, ?, ?, ?, ?)");
+                $insert->bind_param("ssssss", $id, $filename, $kategorie, $path, $date, $benutzerId);
+                $insert->execute();
             }
         }
     } else {
